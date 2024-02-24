e171d546bf083c5e36568d4f71280030cca956c0
Pasindu Heshan <pasindhuheshan@gmail.com>
2024-02-24 19:20:31 +0530

    File Structure

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" href="./assets/imgs/logo.webp" type="image/icon type">
+    <title>Job Recruiter</title>
+  </head>
+  <body>
+  
+  
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid d-flex justify-content-between">
+        <a class="navbar-brand" href="#">Job Recruiter online</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-2">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary rounded-pill" href="#">Sign In</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-dark border border-light rounded-pill" href="#">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="page my-3" id="welcome">
+      <div class="row">
+        <div class="col-md-6 d-flex align-items-center">
+          <h1 class="text-center p-3 ps-5 fs-1">Welcome to Job Recruiter online!</h1>
+        </div>
+        <div class="col-md-6 fs-3 p-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quis voluptatibus officiis porro hic iusto quia quos aliquid animi, aliquam qui quas esse est cum adipisci nobis provident. Magni tempore, molestiae quam accusamus voluptatibus non natus reiciendis, veniam esse, saepe numquam ad optio. Illo et earum rerum velit odit tempore.
+        </div>
+      </div>
+      <hr>
+    </div>
+
+
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
